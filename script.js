3b10886d811ab6c4f34e440b2f2fe0257eb53d77
aasiagithup <132499880+aasiagithup@users.noreply.github.com>
2023-06-10 02:45:00 +0500

    1 file modified

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 /** Implement a click function to which writes digits on calculator screen */
-var screen = document.getElementById("screen");
-function handleclick(digit){
-  screen.textcontent +=digit; 
-}
-var btn1 = document.getElementsByClassName("btn-1");
-function(){
-  handleclick("1")
-}
-  /** Implement a function which clears digits from calculator screen upon  AC button */
-  function clearcalculationscreen(){
-    var ACbutton = document.getElementById("Ac-button");
-    ACbutton.addEventListener("click",clearcalculationscreen);
-    
-  }
-  
-/** Implement a function which enters digit to screen upon pressing any number button and decimal point */
+screen = document.getElementById("screen");let
+buttons = document.querySelectorAll('button');
+String = "";
+arr = Array.from(buttons);
+arr.forEach(button => {
+button.addEventListener("click",(e)=>{
+  if(e.target.innerHTML == '='){
+    string = eval(string);
+    input.value =string;
 
+
+  }
+    string += e.target.innerHTML;
+    
+
+  })
+
+})
+
+
+  /** Implement a function which clears digits from calculator screen upon  AC button */
+
+
+
+ /** Implement a function which enters digit to screen upon pressing any number button and decimal point */
+
+
+ 
 /** Implement a function for adding/subtracting/multiplying/dividing first entered number */
 
 /** */
